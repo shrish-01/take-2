1b016fbc4cf71555624586185430ddf775ee5d6a
Shrish Dollin <shrish.dollin1@gmail.com>
2024-12-30 16:24:00 +0000

    clean slate

--- week-4/dom-react/src/App.jsx
+++ week-4/dom-react/src/App.jsx
@@ -2,14 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { set } from 'zod'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <CustomButton count={count} setCount={setCount}></CustomButton>
+  )
+}
+
+function CustomButton(props) {
+
+  function onClickHandler() {
+    props.setCount(props.count + 1);
+  }
+
+  return (
     <>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
+      <button onClick={onClickHandler}>
+        Counter {props.count}
       </button>
     </>
   )
