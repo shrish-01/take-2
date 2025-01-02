af65ebb81463fba0c4e4c9ede4a56546e2b4c508
Shrish Dollin <shrish.dollin1@gmail.com>
2025-01-02 18:07:59 +0000

    user route

--- week-4/mongo-assignment/routes/user.js
+++ week-4/mongo-assignment/routes/user.js
@@ -1,10 +1,40 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User } = require("../db");
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement user signup logic
+    const { username, password } = req.body;
+
+    try {
+        const userExists = await User.findOne({
+            username
+        });
+    
+        if(userExists) {
+            res.status(409).json({
+                msg: "User already exists!"
+            });
+            return;
+        }
+    
+        const newUser = new User({
+            username: username,
+            password: password
+        });
+    
+        const savedUser = await newUser.save();
+        res.status(200).json({
+            msg: "User created & saved!",
+            id: savedUser._id,
+        });
+    } catch (error) {
+        res.status(500).json({
+            msg: "Something went wrong!", error: error.message
+        });
+    }
 });
 
 router.get('/courses', (req, res) => {
