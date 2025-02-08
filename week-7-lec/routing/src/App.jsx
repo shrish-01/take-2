d7eb6e4395d011415cf9e59ccf11f916d8b5dfa2
Shrish Dollin <shrish.dollin1@gmail.com>
2025-02-08 01:18:41 +0000

    understanding prop drilling

--- week-7-lec/routing/src/App.jsx
+++ week-7-lec/routing/src/App.jsx
@@ -1,22 +1,50 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Landing from './components/Landing';
+import { useState } from 'react';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Landing from './components/Landing';
 // import Dashboard from './components/Dashboard';
-const Dashboard = React.lazy(() => import('./components/Dashboard'));
-import Header from './components/Header';
-import { Suspense } from 'react';
+// const Dashboard = React.lazy(() => import('./components/Dashboard'));
+// import Header from './components/Header';
+// import { Suspense } from 'react';
 
 function App() {
 
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      <BrowserRouter>
+
+      <Count count={count}/>
+      <Buttons count={count} setCount={setCount}/>
+
+      {/* <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/dashboard" element={<Suspense fallback={"loading..."}><Dashboard /></Suspense>} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+    </div>
+  )
+}
+
+function Count({count}) {
+  return <div>{count}</div>
+}
+
+function Buttons({count, setCount}) {
+  return (
+    <div>
+      <button onClick={
+        function() {
+          setCount(count + 1);
+        }
+      }>Increase</button>
+      <button onClick={
+        function() {
+          setCount(count - 1);
+        }
+      }>Decrease</button>
     </div>
   )
 }
